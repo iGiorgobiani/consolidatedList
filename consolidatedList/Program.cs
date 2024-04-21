1e31decef044e5f42c33b0015e8199a8d0419572
Irakli Giorgobiani <giorgobiani.ir@gmail.com>
2024-04-21 22:43:25 +0400

    1. იწერს საიტიდან სანქციადაკისრებული პირების სიას .xml ფორმატში (დაკომენტარებულია დროებით) 2. ხსნის ამ ფაილს 3. ქმნის ინდივიდების სიას 4. პოულობს ფაილში ინდივიდების შესახებ ინფორმაციას და სათითაოდ ათავსებს ინდივიდების სიაში 5. ითვლის ინდივიდებს 6. წერს ინდივიდების აიდის და სახელებს

--- consolidatedList/Program.cs
+++ consolidatedList/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Xml;
+using System.Xml.Linq;
 using unList.Individual;
-
-
-listDownloader.DownloadFile(); // Downloading consolidated list from EU url
 
 List<Individual> individuals = new List<Individual>(); //creation of list of individuals
 Individual individual1 = new Individual()
@@ -29,6 +28,51 @@ Individual individual1 = new Individual()
     PLACE_OF_BIRTH = "New York, USA"
 }; //Tester individual1
 individuals.Add(individual1); // Add individual1 to the list
+int individualsAmount = 0; //Using later to count individuals
+
+//listDownloader.DownloadFile(); // Downloading consolidated list from EU url
+
+
+string filePath = @"C:\Users\User\OneDrive\Desktop\ConsolidateDlistApp\Consolidated.xml";
+
+// Load the XML document
+XDocument xmlDoc = XDocument.Load(filePath);
+
+XElement rootElement = xmlDoc.Root; //Adresses the root element = CONSOLIDATED_LIST
+
+
+foreach (XElement element in rootElement.Elements())
+{
+    if (element.Name == "INDIVIDUALS")
+    {
+        foreach (XElement individualElement in element.Elements("INDIVIDUAL"))
+        {
+            Individual individual = new Individual()
+            {
+                DATAID = individualElement.Element("DATAID")?.Value,
+                VERSIONNUM = individualElement.Element("VERSIONNUM")?.Value,
+                FIRST_NAME = individualElement.Element("FIRST_NAME")?.Value,
+                SECOND_NAME = individualElement.Element("SECOND_NAME")?.Value,
+                THIRD_NAME = individualElement.Element("THIRD_NAME")?.Value,
+                UN_LIST_TYPE = individualElement.Element("UN_LIST_TYPE")?.Value,
+                REFERENCE_NUMBER = individualElement.Element("REFERENCE_NUMBER")?.Value,
+                LISTED_ON = individualElement.Element("LISTED_ON")?.Value,
+                NAME_ORIGINAL_SCRIPT = individualElement.Element("NAME_ORIGINAL_SCRIPT")?.Value,
+                COMMENTS1 = individualElement.Element("COMMENTS1")?.Value,
+                TITLE = individualElement.Element("TITLE")?.Value,
+                DESIGNATION = individualElement.Element("DESIGNATION")?.Value,
+                NATIONALITY = individualElement.Element("NATIONALITY")?.Value,
+                LIST_TYPE = individualElement.Element("LIST_TYPE")?.Value,
+                LIST_DAY_UPDATED = individualElement.Element("LIST_DAY_UPDATED")?.Value,
+                INDIVIDUAL_ADDRESS = individualElement.Element("INDIVIDUAL_ADDRESS")?.Value,
+                DATE_OF_BIRTH = individualElement.Element("DATE_OF_BIRTH")?.Value,
+                PLACE_OF_BIRTH = individualElement.Element("PLACE_OF_BIRTH")?.Value
+            };
+            individuals.Add(individual);
+            individualsAmount++;
+        }
+    }
+}
 
 
 PrintIndividuals(individuals);
@@ -46,10 +90,10 @@ PrintIndividuals(individuals);
 
 
 
-static void PrintIndividuals(List<Individual> individuals)
+void PrintIndividuals(List<Individual> individuals)
 {
     Console.WriteLine();
-    Console.WriteLine("Current individuals list:\n");
+    Console.WriteLine($"Current individuals list: {individualsAmount}\n");
     Console.WriteLine("--------------------------------------------");
 
     foreach (var individual in individuals)
