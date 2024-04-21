9526366294ced3a218ab1d07133dd2cf205d9eac
Irakli Giorgobiani <giorgobiani.ir@gmail.com>
2024-04-22 02:15:49 +0400

    * ENTITIES ADDED * PrintList Added

--- consolidatedList/Individuals.cs
+++ consolidatedList/Individuals.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace unList.Individual
+namespace Lists 
 {
     public class Individual //Creating class of individual based on data of EU xml file
     {
@@ -27,5 +27,20 @@ namespace unList.Individual
         public string INDIVIDUAL_ADDRESS { get; set; }
         public string DATE_OF_BIRTH { get; set; }
         public string PLACE_OF_BIRTH { get; set; }
+    }
+
+    public class Entity //Creating class of entity based on data of EU xml file
+    {
+        public string DATAID { get; set; }
+        public string VERSIONNUM { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string UN_LIST_TYPE { get; set; }
+        public string REFERENCE_NUMBER { get; set; }
+        public string LISTED_ON { get; set; }
+        public string COMMENTS1 { get; set; }
+        public string LIST_TYPE { get; set; }
+        public string LIST_DAY_UPDATED { get; set; }
+        public string ENTITY_ALIAS { get; set; } //ALIAS & ADDRESS MAY BE MORE THAN 1, needs to be addressed later
+        public string ENTITY_ADDRESS { get; set; }
     }
 }
--- consolidatedList/Program.cs
+++ consolidatedList/Program.cs
@@ -2,101 +2,152 @@
 using System.Net;
 using System.Xml;
 using System.Xml.Linq;
-using unList.Individual;
+using Lists;
 
-List<Individual> individuals = new List<Individual>(); //creation of list of individuals
-Individual individual1 = new Individual()
-{
-    DATAID = "12345",
-    VERSIONNUM = "1.0",
-    FIRST_NAME = "John",
-    SECOND_NAME = "Doe",
-    THIRD_NAME = "Smith",
-    UN_LIST_TYPE = "Terrorist",
-    REFERENCE_NUMBER = "UN123456",
-    LISTED_ON = "2023-04-10",
-    NAME_ORIGINAL_SCRIPT = "John Doe Smith",
-    COMMENTS1 = "Suspected terrorist activity",
-    TITLE = "Mr.",
-    DESIGNATION = "Leader",
-    NATIONALITY = "US",
-    LIST_TYPE = "UN List",
-    LIST_DAY_UPDATED = "2023-04-10",
-    INDIVIDUAL_ALIAS = "Johnny",
-    INDIVIDUAL_ADDRESS = "123 Main St, Anytown, USA",
-    DATE_OF_BIRTH = "1980-01-01",
-    PLACE_OF_BIRTH = "New York, USA"
-}; //Tester individual1
-individuals.Add(individual1); // Add individual1 to the list
-int individualsAmount = 0; //Using later to count individuals
-
-//listDownloader.DownloadFile(); // Downloading consolidated list from EU url
-
-
+//Indicating where file shall be downloaded
 string filePath = @"C:\Users\User\OneDrive\Desktop\ConsolidateDlistApp\Consolidated.xml";
+//Creating lists of individuals and entities
+List<Individual> individuals = new List<Individual>(); 
+List<Entity> entities = new List<Entity>();
+int individualsAmount = 0; //Using later to count individuals
+int entitiesAmount = 0; //Using later to count entities
+int individualsError = 0;
+int entitiesError = 0;
+////////////////////////////////////////////////////
 
+listDownloader.DownloadFile(); // Downloading consolidated list from EU url
 // Load the XML document
 XDocument xmlDoc = XDocument.Load(filePath);
-
 XElement rootElement = xmlDoc.Root; //Adresses the root element = CONSOLIDATED_LIST
 
+createIndividuals(); // Reading from file all entries of individuals and adding them to the list of individuals
+createEntities(); // Reading from file all entries of entities and adding them to the list of entities
 
-foreach (XElement element in rootElement.Elements())
+PrintList(); //Console writing all lists
+
+
+
+void createEntities()
 {
-    if (element.Name == "INDIVIDUALS")
+    foreach (XElement element in rootElement.Elements())
     {
-        foreach (XElement individualElement in element.Elements("INDIVIDUAL"))
+        //Console.WriteLine(element);
+        if (element.Name == "ENTITIES")
         {
-            Individual individual = new Individual()
+            foreach (XElement entityElement in element.Elements("ENTITY"))
             {
-                DATAID = individualElement.Element("DATAID")?.Value,
-                VERSIONNUM = individualElement.Element("VERSIONNUM")?.Value,
-                FIRST_NAME = individualElement.Element("FIRST_NAME")?.Value,
-                SECOND_NAME = individualElement.Element("SECOND_NAME")?.Value,
-                THIRD_NAME = individualElement.Element("THIRD_NAME")?.Value,
-                UN_LIST_TYPE = individualElement.Element("UN_LIST_TYPE")?.Value,
-                REFERENCE_NUMBER = individualElement.Element("REFERENCE_NUMBER")?.Value,
-                LISTED_ON = individualElement.Element("LISTED_ON")?.Value,
-                NAME_ORIGINAL_SCRIPT = individualElement.Element("NAME_ORIGINAL_SCRIPT")?.Value,
-                COMMENTS1 = individualElement.Element("COMMENTS1")?.Value,
-                TITLE = individualElement.Element("TITLE")?.Value,
-                DESIGNATION = individualElement.Element("DESIGNATION")?.Value,
-                NATIONALITY = individualElement.Element("NATIONALITY")?.Value,
-                LIST_TYPE = individualElement.Element("LIST_TYPE")?.Value,
-                LIST_DAY_UPDATED = individualElement.Element("LIST_DAY_UPDATED")?.Value,
-                INDIVIDUAL_ADDRESS = individualElement.Element("INDIVIDUAL_ADDRESS")?.Value,
-                DATE_OF_BIRTH = individualElement.Element("DATE_OF_BIRTH")?.Value,
-                PLACE_OF_BIRTH = individualElement.Element("PLACE_OF_BIRTH")?.Value
-            };
-            individuals.Add(individual);
-            individualsAmount++;
+                Entity entity = new Entity()
+                {
+                    DATAID = entityElement.Element("DATAID")?.Value,
+                    VERSIONNUM = entityElement.Element("VERSIONNUM")?.Value,
+                    FIRST_NAME = entityElement.Element("FIRST_NAME")?.Value,
+                    UN_LIST_TYPE = entityElement.Element("UN_LIST_TYPE")?.Value,
+                    REFERENCE_NUMBER = entityElement.Element("REFERENCE_NUMBER")?.Value,
+                    LISTED_ON = entityElement.Element("LISTED_ON")?.Value,
+                    COMMENTS1 = entityElement.Element("COMMENTS1")?.Value,
+                    LIST_TYPE = entityElement.Element("LIST_TYPE")?.Value,
+                    LIST_DAY_UPDATED = entityElement.Element("LIST_DAY_UPDATED")?.Value,
+                    ENTITY_ALIAS = entityElement.Element("ENTITY_ALIAS")?.Value,
+                    ENTITY_ADDRESS = entityElement.Element("ENTITY_ADDRESS")?.Value
+                };
+                entities.Add(entity);
+                entitiesAmount++;
+            }
+        }
+        else
+        {
+            entitiesError++;
         }
     }
 }
-
-
-PrintIndividuals(individuals);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void createIndividuals()
+{
+    foreach (XElement element in rootElement.Elements())
+    {
+        if (element.Name == "INDIVIDUALS")
+        {
+            foreach (XElement individualElement in element.Elements("INDIVIDUAL"))
+            {
+                Individual individual = new Individual()
+                {
+                    DATAID = individualElement.Element("DATAID")?.Value,
+                    VERSIONNUM = individualElement.Element("VERSIONNUM")?.Value,
+                    FIRST_NAME = individualElement.Element("FIRST_NAME")?.Value,
+                    SECOND_NAME = individualElement.Element("SECOND_NAME")?.Value,
+                    THIRD_NAME = individualElement.Element("THIRD_NAME")?.Value,
+                    UN_LIST_TYPE = individualElement.Element("UN_LIST_TYPE")?.Value,
+                    REFERENCE_NUMBER = individualElement.Element("REFERENCE_NUMBER")?.Value,
+                    LISTED_ON = individualElement.Element("LISTED_ON")?.Value,
+                    NAME_ORIGINAL_SCRIPT = individualElement.Element("NAME_ORIGINAL_SCRIPT")?.Value,
+                    COMMENTS1 = individualElement.Element("COMMENTS1")?.Value,
+                    TITLE = individualElement.Element("TITLE")?.Value,
+                    DESIGNATION = individualElement.Element("DESIGNATION")?.Value,
+                    NATIONALITY = individualElement.Element("NATIONALITY")?.Value,
+                    LIST_TYPE = individualElement.Element("LIST_TYPE")?.Value,
+                    LIST_DAY_UPDATED = individualElement.Element("LIST_DAY_UPDATED")?.Value,
+                    INDIVIDUAL_ADDRESS = individualElement.Element("INDIVIDUAL_ADDRESS")?.Value,
+                    DATE_OF_BIRTH = individualElement.Element("DATE_OF_BIRTH")?.Value,
+                    PLACE_OF_BIRTH = individualElement.Element("PLACE_OF_BIRTH")?.Value
+                };
+                individuals.Add(individual);
+                individualsAmount++;
+            }
+        }
+        else
+        {
+            individualsError++;
+        }
+    }
+}
 void PrintIndividuals(List<Individual> individuals)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Errors made when searching for individuals: {individualsError}");
+        Console.WriteLine($"Current individuals list: {individualsAmount}\n");
+        Console.WriteLine("--------------------------------------------");
+
+        foreach (var individual in individuals)
+
+            Console.WriteLine($"{individual.DATAID} {individual.FIRST_NAME} {individual.SECOND_NAME} {individual.THIRD_NAME}");
+    }
+
+void PrintEntities(List<Entity> entities)
 {
     Console.WriteLine();
-    Console.WriteLine($"Current individuals list: {individualsAmount}\n");
+    Console.WriteLine($"Errors made when searching for entities: {entitiesError}");
+    Console.WriteLine($"Current entities list: {entitiesAmount}\n");
     Console.WriteLine("--------------------------------------------");
 
+    foreach (var entity in entities)
+
+        Console.WriteLine($"{entity.DATAID} {entity.FIRST_NAME}");
+}
+
+void PrintList()
+{
+    Console.WriteLine();
+    Console.WriteLine($"Errors made when searching for individuals: {individualsError}");
+    Console.WriteLine($"                                  entities: {entitiesError}");
+    Console.WriteLine($"Current individuals list: {individualsAmount} ||");
+    Console.Write($"                entities: {entitiesAmount}");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($" || TOTAL = {individualsAmount + entitiesAmount}\n");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine("INDIVIDUALS");
+    Console.WriteLine("--------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.White;
     foreach (var individual in individuals)
-     
+    {
         Console.WriteLine($"{individual.DATAID} {individual.FIRST_NAME} {individual.SECOND_NAME} {individual.THIRD_NAME}");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine("ENTITIES");
+    Console.WriteLine("--------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.White;
+    foreach (var entity in entities)
+    {
+        Console.WriteLine($"{entity.DATAID} {entity.FIRST_NAME}");
+    }
 }
